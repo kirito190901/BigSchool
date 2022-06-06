b03628f6699a701007fe3bac5fb51dab1d482ca1
kirit <kirit@DESKTOP-Q7V68H9>
2022-06-06 21:25:09 +0000

    Add project files.

---
+++ BigSchool/Controllers/FollowingController.cs
@@ -0,0 +1,37 @@
+﻿using BigSchool.DTOs;
+using BigSchool.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BigSchool.Controllers
+{
+    public class FollowingController : ApiController
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public FollowingController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        [HttpPost]
+        public IHttpActionResult Follow(FollowingDto followingDto)
+        {
+            var userId = User.Identity.GetUserId();
+            if (_dbContext.Followings.Any(f => f.FollowerId == userId && f.FolloweeId == followingDto.FolloweeId))
+                return BadRequest("The Attendance already exists!");
+            var folowing = new Following
+            {
+                FollowerId = userId,
+                FolloweeId = followingDto.FolloweeId
+            };
+
+            _dbContext.Attendances.Add(folowing);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+    }
+}
